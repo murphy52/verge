6c59b6b8d7b4b7da46b9fd87e44601ee67b584aa
Murphy52 <Murphy52@gmail.com>
2014-04-19 16:48:29 -0400

    Added Bootstrap

--- views/layout.php
+++ views/layout.php
@@ -1,9 +1,39 @@
 <html>
 	<head>
-		<link href="<?php echo $this->make_route('/css/master.css') ?>" rel="stylesheet" type="text/css" />
+		<link href="<?php echo $this->make_route('css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
+		<link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
+		<link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />
+		
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-		<h1>Verge</h1>
-		<?php include($this->content); ?>
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<a class="brand" href="<?php echo $this->make_route('/') ?>">Verge</a>
+					<div class="nav-collapse">
+						<ul class="nav">
+							<li><a href="<?php echo $this->make_route('/') ?>">Home</a>
+							</li>
+							<li><a href="<?php echo $this->make_route('/signup') ?>">Signup</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			
+			<?php include($this->content); ?>
+		
+		</div>
 	</body>
 </html>
